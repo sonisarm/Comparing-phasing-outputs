d8122aa2b363348308fe9bc89e2699b11a8ad481
Sonia Sarmiento <sonia.sarmientocabello@unil.ch>
2023-02-02 16:33:29 +0100

    Update 4_ShapeIt.sh

--- 4_ShapeIt.sh
+++ 4_ShapeIt.sh
@@ -1,12 +1,10 @@
-     ref='/work/FAC/FBM/DEE/jgoudet/barn_owl/Common/ref_genome_2020/Tyto_reference_Jan2020.fasta' # reference path
-        #Extract scaffold files from runLINE
-        scaffold=$1
-	echo ${scaffold}
-  
-  
-# Phasing UNRELATED individuals
-shapeit4 --input ${post-whatshap_filtered.vcf} \
+shapeit4 --input $post-whatshap_filtered.vcf \
  -T 24 \
- -R ${chr} --sequencing \
+ --region $CHR --sequencing \
+ --reference $ref \
+ --use-PS 0.0001 \
  --pbwt-depth 8 --mcmc-iterations 10b,1p,1b,1p,1b,1p,1b,1p,10m \
- --output $phased.vcf
+ --output $phased_CHR.vcf
+
+bcftools view $phased_CHR.vcf -O z -o $phased_CHR.vcf.gz
+bcftools index $phased_CHR.vcf.gz
