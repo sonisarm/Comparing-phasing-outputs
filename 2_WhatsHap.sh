d6e8e2b10aa941b1434516c9351f66a755dd2029
Sonia Sarmiento <sonia.sarmientocabello@unil.ch>
2023-02-02 16:28:26 +0100

    Update 2_WhatsHap.sh

--- 2_WhatsHap.sh
+++ 2_WhatsHap.sh
@@ -1,9 +1,24 @@
-#ped: PED file containing pedigree information of individuals (Offspring ID | Dad ID | Mom ID | Sex). This file is required for pedigree phasing but not for read-base phasing.
+#INPUTS:
 #input VCF is the filtered VCF with unphased haplotypes. It should contain ONLY the individuals to be phased
 #input BAM contains reads of individuals to be phased - must contain at least the individuals that are being phased.
 
+# Read-base phasing
 whatshap phase \
      --ped $ped \
+     -o $phased.vcf \
+     --reference $ref \
+     --indels \
+     $input.vcf.gz \
+     $bam.bam
+
+     # Zip VCF file
+     bcftools view  $phased.vcf -O z -o $phased_zipped.vcf.gz
+     #Index VCF file
+     bcftools index $phased_zipped.vcf.gz
+
+# ADDITIONAL INPUT:
+#ped: PED file containing pedigree information of individuals (Offspring ID | Dad ID | Mom ID | Sex). This file is required for pedigree phasing but not for read-base phasing.
+whatshap phase \
      -o $phased.vcf \
      --reference $ref \
      --indels \
