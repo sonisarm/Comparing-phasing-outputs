cca322f60f90c73b642e27294d8aa1bd466edb31
Sonia Sarmiento <sonia.sarmientocabello@unil.ch>
2023-02-02 16:36:12 +0100

    Update 4_ShapeIt.sh

--- 4_ShapeIt.sh
+++ 4_ShapeIt.sh
@@ -1,8 +1,8 @@
 shapeit4 --input $post-whatshap_filtered.vcf \
- -T 24 \
- --region $CHR --sequencing \
- --reference $ref \
- --use-PS 0.0001 \
+ -T 24 \   #threads
+ --region $CHR --sequencing \ #specifying chromosome/scaffold 
+ --reference $ref \  # reference genome
+ --use-PS 0.0001 \  # taking into account phasing sets (generated during whatshap phasing)
  --pbwt-depth 8 --mcmc-iterations 10b,1p,1b,1p,1b,1p,1b,1p,10m \
  --output $phased_CHR.vcf
 
