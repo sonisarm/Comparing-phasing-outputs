25705490ac9a07a66d013c24a540896658fdda50
Sonia Sarmiento <sonia.sarmientocabello@unil.ch>
2023-02-02 15:26:51 +0100

    Update 1_FilteringVCFs.sh

--- 1_FilteringVCFs.sh
+++ 1_FilteringVCFs.sh
@@ -2,7 +2,7 @@
 gatk VariantFiltration \
 	-R $ref \
 	-V $VCF \
-	-O $intermediateVCF \
+	-O $tmp \
 	--filter-expression "QD < 2.0" --filter-name "QD2" \
 	--filter-expression "FS > 60.0" --filter-name "FS60" \
 	--filter-expression "SOR > 3.0" --filter-name "SOR3" \
@@ -13,25 +13,24 @@ gatk VariantFiltration \
 #Subsetting the selected variants
 gatk SelectVariants \
 	-R $ref \
-	-V $intermediateVCF \
-	-O $filteredVCF \
+	-V $tmp \
+	-O $filtered.vcf \
 	--exclude-filtered true \
 	--exclude-non-variants
 	
 	
 # Remove masked regions
-bcftools view -O z -o $outputVCF -R maskedregions_150_90.bed \
-$filteredVCF.vcf.gz
+bcftools view -O z -o $output.vcf -R maskedregions_150_90.bed $filtered.vcf
 
 #Index bcf
-tabix $outputVCF
+tabix $output.vcf
 
 # Filter variants for sequencing depth (DP)
 gatk VariantFiltration \
--V ${outvcf} \
+-V $output.vcf \
 -G-filter "DP > $cutoffhigh" \
 -G-filter-name 'GDPhigh' \
 -G-filter "DP < $cutofflow" \
 -G-filter-name 'GDPlow' \
--O  ${outvcf2} \
+-O  $output_final.vcf \
 --set-filtered-genotype-to-no-call true
