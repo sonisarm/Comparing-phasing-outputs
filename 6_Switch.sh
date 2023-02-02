29cbf89386c26c67fe11b263378d22e63c87c6cd
Sonia Sarmiento <sonia.sarmientocabello@unil.ch>
2023-02-02 17:12:00 +0100

    Update 6_Switch.sh

--- 6_Switch.sh
+++ 6_Switch.sh
@@ -3,15 +3,18 @@ myPATH=/soniasarmiento/Phasing/Switch
 # Save the code into a variable
 Switch=${myPATH}/SHAPEIT5_switch_static
 
+#Define chromosome/scaffold
 CHR=Super-Scaffold_14
+
 #VAL = file from where Switch computes Mendelian Inheritance and uses as validation
 VAL=UNPHASED_PARENTS_AND_OFFSPRING
-      # PED file of the samples
-      ped=${myPATH}/PEDFile.ped
+# PED file of the samples
+ped=${myPATH}/PEDFile.ped
+
 #EST = VCF file with Phased offspring
 EST=PHASED_OffspringONLY
 #Output prefix:
-OUTPUT_PREFIX=${myPATH}
+OUTPUT_PREFIX=${myPATH}/Output1
 
 #### SHAPEIT5 switch tool ###
 ${Switch} --validation ${VAL}_${CHR}.vcf.gz --estimation ${EST}_${CHR}.vcf.gz --region ${CHR} --pedigree ${ped} --output ${OUTPUT_PREFIX}_${CHR}
